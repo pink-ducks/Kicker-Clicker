368870a294e8c359308692e59a26d502ffb976e6
Mateusz <matpiwowarski7@gmail.com>
2019-03-07 23:24:03 +0100

    MVC idea
    Model–View–Controller

---
+++ WPF App/WPF App/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_App
+{
+    public class User
+    {
+        private int _points = 0;
+        public int Points { get => _points; set => _points = value; }
+        private int _speedOfAddingPoints = 1;
+        public int SpeedOfAddingPoints { get => _speedOfAddingPoints; set => _speedOfAddingPoints = value; }
+
+        // constructor
+        public User()
+        {
+        }
+        // methods
+
+    }
+}
