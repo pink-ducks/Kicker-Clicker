8ad03934b4cb8f2890f1251d4f91f2726e4136c1
krupowies <krupowieswojciech@gmail.com>
2019-03-10 20:52:21 +0100

    SQLconnection
    dodana klasa do połączenia z SQL

---
+++ WPF App/WPF App/Model/ConnSQL.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace WPF_App.Model
+{
+    class ConnSQL
+    {
+        //SqlConnection conn = new SqlConnection("Data Source=DESKTOP-PEJHOGB;Initial Catalog=NowaBaza;Integrated Security=True");
+        public void Connection()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection("Data Source=DESKTOP-PEJHOGB;Initial Catalog=NowaBaza;Integrated Security=True"))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand("SELECT * FROM dbo.Table", connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                for (int i = 0; i < reader.FieldCount; i++)
+                                {
+                                    Console.WriteLine(reader.GetValue(i));
+                                }
+                                Console.WriteLine();
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("can not open connection ! ");
+            }
+        }
+    }
+}
